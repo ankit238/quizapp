5f7c7582c504e7197b7bf44d4e1d51dc6c253701
Ankit Kumar <79011661+ankit238@users.noreply.github.com>
2022-05-16 13:53:34 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by{" "}
+  <h6>ankit kumar</h6>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/sparkle/Sparkle.js
@@ -0,0 +1,14 @@
+const Sparkle = ({ children }) => {
+  return (
+    <div
+      style={{
+       
+        backgroundImage: "url('https: //www.icegif.com/wp-content/uploads/confetti-icegif-2.gif')"
+      }}
+    >
+      <h1>huraay</h1>
+    </div>
+  );
+};
+
+export default Sparkle;
